2202c3be2ce54d15fa526200bce38baec688e385
satan_pig <1048218204@qq.com>
2024-05-04 00:26:39 +0800

    完善Menu组件的导出功能

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,16 @@
+import Menu, { MenuProps } from "./menu";
+import MenuItem, { MenuItemProps } from "./menuItem";
+import { FC } from "react";
+import SubMenu, { SubMenuProps } from "./subMenu";
+// import { Transition } from "react-transition-group";
+export type IMenuComponent = FC<MenuProps> & {
+  Item: FC<MenuItemProps>,
+  SubMenu: FC<SubMenuProps>
+}
+
+const TransMenu = Menu as IMenuComponent
+
+TransMenu.Item = MenuItem
+TransMenu.SubMenu = SubMenu
+
+export default TransMenu;
