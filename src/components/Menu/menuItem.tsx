0987cd66b47276e164438179c9d5fc7f8c45466c
satan_pig <1048218204@qq.com>
2024-05-03 14:38:22 +0800

    菜单选项

---
+++ src/components/Menu/menuItem.tsx
@@ -0,0 +1,36 @@
+import React, { useContext } from "react";
+import { MenuContext } from "./menu";
+import classNames from "classnames";
+
+export interface MenuItemProps {
+  index: string;
+  className?: string;
+  disabled?: boolean;
+  style?:React.CSSProperties
+  children?: React.ReactNode; 
+}
+
+const MenuItem: React.FC<MenuItemProps> = (props) => {
+  const {index, className, disabled, style, children} = props
+  const context = useContext(MenuContext)
+  const classes = classNames('menu-item', className,{
+    'is-disabled': disabled,
+    'is-active': context.index === index
+  })
+
+  const handleClick = () => {
+    // 这里的typeof index === 'string'是为了防止用户在使用时候的运行时错误
+    // 比方说用户给你传了一个number类型作为index，但是你的index是string类型
+    if(context.onSelect && !disabled && (  )){
+      context.onSelect(index)
+    }
+   
+  }
+  return (
+    <li className={classes} style={style} onClick={handleClick}>{children}</li>
+  )
+
+}
+
+MenuItem.displayName = 'MenuItem'
+export default MenuItem
